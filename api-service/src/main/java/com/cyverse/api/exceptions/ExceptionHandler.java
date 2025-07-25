62f9eb6713642fa5dd8f5bf9ac815577f98703c0
enitu <edi.nitu2000@yahoo.com>
2025-07-25 15:59:06 +0200

    irods: ichmod | ldap: add user to groups

--- api-service/src/main/java/com/cyverse/api/exceptions/ExceptionHandler.java
+++ api-service/src/main/java/com/cyverse/api/exceptions/ExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.cyverse.api.exceptions;
 
-import com.cyverse.api.controllers.IrodsController;
 import io.javalin.Javalin;
 import io.javalin.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.naming.NamingException;
 
 
 public class ExceptionHandler {
@@ -20,6 +21,11 @@ public class ExceptionHandler {
         app.exception(UserException.class, (e, ctx) -> {
             logger.error("Bad request error: {}", e.getMessage());
             ctx.status(HttpStatus.BAD_REQUEST);
+        });
+
+        app.exception(NamingException.class, (e, ctx) -> {
+            logger.warn("LDAP Error: {}", e.getMessage());
+            ctx.status(HttpStatus.INTERNAL_SERVER_ERROR);
         });
 
         app.exception(Exception.class, (e, ctx) -> {
