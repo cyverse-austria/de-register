f37eeffc8d9b843a9d9477a80c6723764f79e596
enitu <edi.nitu2000@yahoo.com>
2025-07-30 12:46:31 +0200

    tests: infrastructure for local deployment

---
+++ register-test/README.md
@@ -0,0 +1,44 @@
+# register-test
+
+In this module, CyVerse services bound with Keycloak login are configured and deployed
+automatically for testing purposes.
+
+## Local testing
+
+Run `./run-tests.sh <service> <client-secret>`. Where:
+
+ - service: the CyVerse service to test - currently only **User Portal** supported
+ - client-secret: the client secret for the Keycloak client that is configured for the service
+
+The script will:
+
+- setup the database for the chosen service as well as for Keycloak.
+- setup the 2 de-register services present in this repository and start them:
+api-service, event-listener.
+- additionally, the Java Application script will create a testing realm, a client for the
+service and testing users.
+- will start the configured service.
+- additional manual steps may still be required. These will be prompted at the end of the script.
+
+## Structure
+
+This directory structure must be respected in order for the script to work.
+
+```
+root-dir
+|- de-register
+|    |- event-listener
+|    |- api-service
+|    |- register-test
+|- portal2
+|- portal2-db
+```
+
+portal2: https://github.com/cyverse-austria/portal2 OR https://github.com/cyverse-de/portal2
+
+portal2-db: https://github.com/cyverse-austria/portal2-db
+
+## Long term
+For now this module is used only to setup and configure the requiered services and plugins 
+in order to test the whole de-register workflow. In the future this module may evolve in a
+fully automated integration testing module.
---
+++ register-test/src/main/java/Application.java
@@ -0,0 +1,77 @@
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+
+import java.util.List;
+
+public class Application {
+
+    public static String TEST_REALM = "testrealm";
+
+    public static void main(String[] args) {
+        try (Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl("http://localhost:8087")
+                .realm("master")
+                .clientId("admin-cli")
+                .username("admin")
+                .password("admin")
+                .build()) {
+
+            System.out.println("Creating new realm: testrealm");
+            RealmRepresentation rr = new RealmRepresentation();
+            rr.setId(TEST_REALM);
+            rr.setRealm(TEST_REALM);
+            rr.setEnabled(true);
+
+            keycloak.realms().create(rr);
+            System.out.println("Realm creation: done");
+
+            System.out.println("Adding new client: " + args[0]);
+            String name = args[0];
+            String port = args[1];
+            String secret = args[2];
+
+            keycloak.realm(TEST_REALM).clients().create(getClient(name, port, secret));
+            System.out.println("Client add: done");
+
+            System.out.println("Adding test users");
+            keycloak.realm(TEST_REALM).users().create(getTestUser("test_user1"));
+            keycloak.realm(TEST_REALM).users().create(getTestUser("test_user2"));
+            System.out.println("Testing users: done");
+        }
+    }
+
+    private static ClientRepresentation getClient(String name, String port, String secret) {
+        ClientRepresentation client = new ClientRepresentation();
+        client.setClientId(name);
+        String baseURL = "http://127.0.0.1:" + port;
+        client.setRootUrl(baseURL);
+        client.setAdminUrl(baseURL);
+        client.setWebOrigins(List.of(baseURL));
+        client.setRedirectUris(List.of(baseURL + "/*"));
+        client.setSecret(secret);
+        client.setProtocol("openid-connect");
+        client.setStandardFlowEnabled(true);
+        client.setEnabled(true);
+    }
+
+    private static UserRepresentation getTestUser(String username) {
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue("123456");
+        credential.setTemporary(false);
+
+        UserRepresentation user = new UserRepresentation();
+        user.setUsername(username);
+        user.setFirstName(username);
+        user.setLastName("test_firstname");
+        user.setEmail(username + "@gmail.com");
+        user.setCredentials(List.of(credential));
+        user.setEnabled(true);
+
+        return user;
+    }
+}
