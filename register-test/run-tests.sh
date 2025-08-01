f94ce01edaa85ce1b34434cbcfc4e6a12444b591
Eduard Nitu <edi.nitu2000@yahoo.com>
2025-08-01 13:27:32 +0200

    tests: infrastructure for local deployment (#15)
    * tests: infrastructure for local deployment
    
    * ldap: force account linking
    
    * tests: update testing
    
    * Revert "ldap: force account linking"
    
    This reverts commit 755afe413d83fba8493ac1b824e0fb0430f35bdf.
    
    * tests: add LDAP creation

---
+++ register-test/run-tests.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+  echo "USAGE: ./run-tests.sh <service> <client-secret>"
+  exit 1
+fi
+
+api_container_id=$(docker ps | grep api | awk '{print $1}')
+if [ -n "$api_container_id" ]; then
+  echo "Stopping previous api-service"
+  docker stop $api_container_id
+fi
+
+echo "Start api-service"
+docker build --build-arg APP_VERSION=0.0.1 -t api-service ../api-service/
+docker run --rm -it \
+ -e IRODS_HOST=qa-ies.cyverse.at -e IRODS_USER_NAME=portal -e IRODS_ZONE_NAME=TUG -p 7000:7000 -d api-service api
+
+docker compose down
+docker compose up -d --build
+
+db_container_id=$(docker ps | grep db | awk '{print $1}')
+
+if [ -z "$db_container_id" ]; then
+  echo "Containers did not start correctly."
+  exit 1
+fi
+
+until docker exec -i $db_container_id psql -U postgres -c '\l' > /dev/null 2>&1; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 1
+done
+
+echo "Creating Keycloak DB"
+docker exec -i $db_container_id psql -U postgres -c "create user keycloak with password 'keycloak';"
+docker exec -i $db_container_id psql -U postgres -c "create database keycloak with owner keycloak;"
+docker exec -i $db_container_id psql -U postgres -c "grant postgres to keycloak;"
+
+echo "Waiting for Keycloak to initialize"
+sleep 20
+
+mvn clean package
+
+
+if [ "$1" = "portal" ]; then
+
+  echo "Creating Keycloak setup: realm, client, users."
+  java -jar target/register-test.jar ./src/main/resources/test-config.yml portal 3000 $2
+
+  docker exec -i $db_container_id psql -U postgres -c "create user portal_db_reader with password 'admin';"
+  docker exec -i $db_container_id psql -U postgres -c "create database portal with owner portal_db_reader;"
+  docker exec -i $db_container_id psql -U postgres -c "grant postgres to portal_db_reader;"
+
+  cd ../../portal2
+  docker run --rm \
+    -v ./migrations:/migrations \
+    --network host \
+    migrate/migrate \
+    --database "postgres://portal_db_reader:admin@localhost/portal?sslmode=disable" \
+    -path /migrations \
+    up
+
+  echo "Importing portal institutions"
+  cd ./src/scripts
+  rm -rf .venv
+  mkdir .venv
+  uv venv .venv
+  source .venv/bin/activate
+  uv pip install psycopg2-binary
+  python import_grid_institutions.py --host localhost --user portal_db_reader --database portal grid.csv
+  deactivate
+
+  echo "Importing portal metadata"
+  cd ../../../portal2-db
+  cd sqls
+  for file in ./*.sql; do
+    docker exec -i $db_container_id psql -U portal_db_reader -d portal < $file
+  done
+
+  cd ../../portal2
+
+  echo "Will start portal app. To finish the setup, please register users with usernames: test_user1, test_user2"
+  echo "Please use this script with care. Calling it again will drop all changes and start the setup from scratch."
+  echo "If you stop this run, then go to portal2 directory. There run <npm run dev>."
+  npm run dev
+fi
+# TODO Add discovery-environment workflow?
---
+++ register-test/src/main/java/Application.java
@@ -0,0 +1,158 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import javax.ws.rs.core.Response;
+import org.apache.http.HttpStatus;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.common.util.MultivaluedHashMap;
+import org.keycloak.representations.idm.*;
+
+import java.io.File;
+import java.util.List;
+
+public class Application {
+
+    public static class Config {
+        public String kcUrl;
+        public String ldapUrl;
+        public String ldapBindDn;
+        public String ldapAdminPasswd;
+        public String ldapUsersDn;
+    }
+
+    public static String TEST_REALM = "testrealm";
+
+    public static void main(String[] args) {
+        Config appConfig = new Config();
+        try {
+            appConfig = loadConfig(args[0]);
+        } catch (Exception e) {
+            System.out.println("Failed loading config. " + e.getMessage());
+            System.exit(1);
+        }
+
+        try (Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl(appConfig.kcUrl)
+                .realm("master")
+                .clientId("admin-cli")
+                .username("admin")
+                .password("admin")
+                .build()) {
+
+            System.out.println("Creating new realm: testrealm");
+            RealmRepresentation rr = new RealmRepresentation();
+            rr.setId(TEST_REALM);
+            rr.setRealm(TEST_REALM);
+            rr.setEnabled(true);
+
+            keycloak.realms().create(rr);
+            System.out.println("Realm creation: done");
+
+            System.out.println("Adding new client: " + args[1]);
+            String name = args[1];
+            String port = args[2];
+            String secret = args[3];
+
+            Response clientResp =
+                    keycloak.realm(TEST_REALM)
+                            .clients()
+                            .create(getClient(name, port, secret));
+            logResponse(clientResp);
+            System.out.println("Client add: done");
+
+            System.out.println("Adding test users");
+            keycloak.realm(TEST_REALM).users().create(getTestUser("test_user1"));
+            keycloak.realm(TEST_REALM).users().create(getTestUser("test_user2"));
+            System.out.println("Testing users: done");
+
+            System.out.println("Adding LDAP Federation");
+            RealmRepresentation repr = keycloak.realm(TEST_REALM).toRepresentation();
+            Response ldapResp = keycloak.realm(TEST_REALM)
+                    .components()
+                    .add(getLdapComponent(
+                            repr.getId(),
+                            appConfig));
+            logResponse(ldapResp);
+            System.out.println("LDAP Federation: done");
+        }
+    }
+
+    private static void logResponse(Response resp) {
+        System.out.println("Request status: " + resp.getStatus());
+        if (resp.getStatus() != HttpStatus.SC_CREATED) {
+            System.out.println("Got HTTP Exception from Keycloak server:");
+            System.out.println(resp.readEntity(String.class));
+        }
+    }
+
+    private static ComponentRepresentation getLdapComponent(String parentId, Config config) {
+
+        ComponentRepresentation ldapComponent = new ComponentRepresentation();
+        ldapComponent.setName("ldap");
+        ldapComponent.setProviderId("ldap");
+        ldapComponent.setProviderType("org.keycloak.storage.UserStorageProvider");
+        ldapComponent.setParentId(parentId);
+
+        MultivaluedHashMap<String, String> ldapParams = new MultivaluedHashMap<>();
+        ldapParams.put("enabled", List.of("true"));
+        ldapParams.put("priority", List.of("0"));
+        ldapParams.put("editMode", List.of("READ_ONLY"));
+        ldapParams.put("importEnabled", List.of("true"));
+        ldapParams.put("syncRegistrations", List.of("false"));
+        ldapParams.put("vendor", List.of("other"));
+        ldapParams.put("usernameLDAPAttribute", List.of("uid"));
+        ldapParams.put("rdnLDAPAttribute", List.of("uid"));
+        ldapParams.put("uuidLDAPAttribute", List.of("entryUUID"));
+        ldapParams.put("userObjectClasses", List.of("inetOrgPerson, organizationalPerson"));
+        ldapParams.put("connectionUrl", List.of(config.ldapUrl));
+        ldapParams.put("usersDn", List.of(config.ldapUsersDn));
+        ldapParams.put("bindDn", List.of(config.ldapBindDn));
+        ldapParams.put("bindCredential", List.of(config.ldapAdminPasswd));
+        ldapParams.put("searchScope", List.of("1"));
+        ldapParams.put("useTruststoreSpi", List.of("ldapsOnly"));
+
+        ldapComponent.setConfig(ldapParams);
+
+        return ldapComponent;
+    }
+
+
+    private static ClientRepresentation getClient(String name, String port, String secret) {
+        ClientRepresentation client = new ClientRepresentation();
+        client.setClientId(name);
+        String baseURL = "http://127.0.0.1:" + port;
+        client.setRootUrl(baseURL);
+        client.setAdminUrl(baseURL);
+        client.setWebOrigins(List.of(baseURL));
+        client.setRedirectUris(List.of(baseURL + "/*"));
+        client.setSecret(secret);
+        client.setProtocol("openid-connect");
+        client.setStandardFlowEnabled(true);
+        client.setEnabled(true);
+
+        return client;
+    }
+
+    private static UserRepresentation getTestUser(String username) {
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue("123456");
+        credential.setTemporary(false);
+
+        UserRepresentation user = new UserRepresentation();
+        user.setUsername(username);
+        user.setFirstName(username);
+        user.setLastName("test_firstname");
+        user.setEmail(username + "@gmail.com");
+        user.setCredentials(List.of(credential));
+        user.setEnabled(true);
+
+        return user;
+    }
+
+    private static Config loadConfig(String filePath) throws Exception {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(new File(filePath), Config.class);
+    }
+}
