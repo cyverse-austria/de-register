076976074fa58cccb70e88be843a7babe62d9831
enitu <edi.nitu2000@yahoo.com>
2025-08-07 10:33:19 +0200

    tests: ldap service tests

--- api-service/src/main/java/com/cyverse/api/config/GenericConfig.java
+++ api-service/src/main/java/com/cyverse/api/config/GenericConfig.java
@@ -2,6 +2,9 @@ package com.cyverse.api.config;
 
 import com.cyverse.api.exceptions.ConfigException;
 
+/**
+ * Base config interface for all api-service configs.
+ */
 public interface GenericConfig {
     void verifyFieldsAreSet() throws ConfigException;
 }
