6d5759bae9ccd84ba7a72924daa41c63bfac66fb
Eduard Nitu <edi.nitu2000@yahoo.com>
2026-01-27 10:21:22 +0100

    fix: disable user portal actions and api

--- api-service/src/main/java/com/cyverse/api/Application.java
+++ api-service/src/main/java/com/cyverse/api/Application.java
@@ -118,8 +118,8 @@ public class Application {
         app.put("/api/groups/ldap", ldapController::addLdapUserToGroup);
         app.put("/api/groups/ldap/password", ldapController::addPasswordToLdapUser);
 
-        UserPortalController userPortalController = new UserPortalController(userPortalService);
-        app.post("/api/users/portal", userPortalController::addUserPortalUser);
+        // UserPortalController userPortalController = new UserPortalController(userPortalService);
+        // app.post("/api/users/portal", userPortalController::addUserPortalUser);
     }
 
     private static ApiServiceConfig loadConfig(String filePath) throws Exception {
